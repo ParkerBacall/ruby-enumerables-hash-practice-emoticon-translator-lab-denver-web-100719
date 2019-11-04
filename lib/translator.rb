5d1ed61abf8b8ce7b6570af77662b4f14f7ae4da
Parker Bacall <pbacall2@gmail.com>
2019-11-04 01:45:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   emoticons.each do |meanings, array|
     array.each do |emoticon|
       hash[:get_meaning]
-      pp meanign
+      pp meanings
     end
   end
   pp hash
