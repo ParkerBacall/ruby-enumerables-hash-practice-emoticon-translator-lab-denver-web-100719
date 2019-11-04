194eaea01677714cf44a91174f363b66219fd636
Parker Bacall <pbacall2@gmail.com>
2019-11-04 02:09:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,8 @@ require "yaml" #require what lets yaml work
 def load_library(file_path) #loads in a file path
   hash = {get_meaning: {}, get_emoticon: {}} # hash that will be returned has keys get meaning and get emoticons that point to empty hashes
   emoticons = YAML.load_file(file_path) #sets the yaml file to vriable emoticons
-  emoticons.each do |meanings, array| #splits yaml file up into meanings keys and arrays that they point to 
-  hash[:get_meaning]["#{array[1]}"] = "#{meanings}"
+  emoticons.each do |meanings, array| #splits yaml file up into meanings keys and arrays that they point to
+  hash[:get_meaning]["#{array[1]}"] = "#{meanings}" #sets key get_meaning equal to 
   hash[:get_emoticon]["#{array[0]}"] = "#{array[1]}"
 end
   hash
