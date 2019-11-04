dd9613394a7bbb05eb7e2923ad2f113fab38e45d
Parker Bacall <pbacall2@gmail.com>
2019-11-04 02:03:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def get_japanese_emoticon(file_path, emoticon)
   if load_library(file_path)[:get_emoticon][emoticon]
     return load_library(file_path)[:get_emoticon][emoticon]
   else
-    return nil
+    return  "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
