03a6d5763aeb9f6eaf90e076402980f0b4f0175c
Parker Bacall <pbacall2@gmail.com>
2019-11-04 01:36:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   emoticons.each do |meanings, array|
     array.each do |emoticon|
-      hash[get_meaning: meanings , get_emoticon: emoticons]
+      hash[get_meaning: meanings]
     end
   end
   pp hash
