f5aa21bcd397504614c6a33f08f9d76b04084aa9
Parker Bacall <pbacall2@gmail.com>
2019-11-04 01:38:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,11 @@
 # require modules here
 require "yaml"
 def load_library(file_path)
-  hash = {}
+  hash = {:get_meaning: 0, get_emoticon:}
   emoticons = YAML.load_file(file_path)
   emoticons.each do |meanings, array|
     array.each do |emoticon|
-      hash[get_meaning: nil]
-      hash[get_emoticon: nil]
+
     end
   end
   pp hash
