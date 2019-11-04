587ae8a0cbf53e30e61dddac32a38ddf61fd5578
Parker Bacall <pbacall2@gmail.com>
2019-11-04 02:11:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,8 @@ def load_library(file_path) #loads in a file path
   hash = {get_meaning: {}, get_emoticon: {}} # hash that will be returned has keys get meaning and get emoticons that point to empty hashes
   emoticons = YAML.load_file(file_path) #sets the yaml file to vriable emoticons
   emoticons.each do |meanings, array| #splits yaml file up into meanings keys and arrays that they point to
-  hash[:get_meaning]["#{array[1]}"] = "#{meanings}" #sets key get_meaning equal to 
-  hash[:get_emoticon]["#{array[0]}"] = "#{array[1]}"
+  hash[:get_meaning]["#{array[1]}"] = "#{meanings}" #populates the empty hash that get_meaning points to with a key which is the japanese emoticon to the meaning
+  hash[:get_emoticon]["#{array[0]}"] = "#{array[1]}" # populates the empty hash that ger_emoticon with a key which is the english emoticon that points to the japanese emoticon
 end
   hash
 end
